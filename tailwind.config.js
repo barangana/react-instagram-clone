a8394dc35de33e0b85a2232fa07193891eebab90
Adriel Barangan <baranganadriel@gmail.com>
2021-03-31 23:46:35 -0400

    created login form

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,36 @@
 module.exports = {
-  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
-  darkMode: false, // or 'media' or 'class'
+  future: {
+    removeDeprecatedGapUtilities: true,
+  },
+  purge: {
+    content: ["./src/**/*.js", "./src/**/**/*.js"],
+  },
   theme: {
-    extend: {},
+    fill: (theme) => ({
+      red: theme("colors.red.primary"),
+    }),
+    colors: {
+      white: "#ffffff",
+      blue: {
+        medium: "#005c98",
+      },
+      black: {
+        light: "#262626",
+        faded: "#00000059",
+      },
+      gray: {
+        base: "#616161",
+        background: "#fafafa",
+        primary: "#dbdbdb",
+      },
+      red: {
+        primary: "#ed4956",
+      },
+    },
   },
   variants: {
-    extend: {},
+    extend: {
+      display: ["group-hover"],
+    },
   },
-  plugins: [],
 };
